70639ca913c2b84067c03d6cfaf05a428dbd3a63
Sam Newman <sonewman@users.noreply.github.com>
2014-10-01 12:07:56 +0100

    finished unfinished comment

--- certain.js
+++ certain.js
@@ -172,7 +172,7 @@ inherits(ResultsStream, Readable)
 
 ResultsStream.prototype._read = function () {}
 
-// certain assertions with a
+// certain assertions with a results readable stream
 function CertainResults(resultsStream, value) {
   Certain.call(this, value)
   this.__resultStream__ = resultsStream
